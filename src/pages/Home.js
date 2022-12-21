d2b90020ece20a7341c9d074ce1edb99355f6096
Josh Levine <levine.joshuaadam@gmail.com>
2022-12-21 11:41:16 -0500

    projects page initial setup complete. Portfolio deployed to surge

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,12 @@ import { Link } from 'react-router-dom'
 
 import headshot from '../assets/JoshLevine.jpg'
 
+//from michaellackey.com
+// Experience in:
+// ReactJS/Redux, Python3/Django, Node.js/Express, AWS S3, PostgreSQL/Sequelize, MongoDB/Mongoose, Heroku/Netlify/Surge, Git/GitHub, HTML5, CSS3, JavaScript and much more....
+
+// AND a mosaic of icons of tech used?
+
 const Home = () => {
   return (
     <div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,58 +1,63 @@
 // import React from 'react'
 import projectImage from '../assets/projectIcon.png'
+import TttImg from '../assets/TttImg.png'
+import FskImg from '../assets/FskImg.png'
+import SkImg from '../assets/SkImg.png'
+// import headerFSK from '../assets/header_FSK.png'
+// import headerSK from '../assets/header_SK.png'
 import { Link } from 'react-router-dom'
 
 const Projects = () => {
   const projectsList = [
     {
       name: 'Tic-Tac-Toe',
-      image: projectImage,
-      //this github needs CLEANED UP... readme is still from markdown HW!
+      image: TttImg,
       github: 'https://github.com/jadlevine/u1_hw_tic_tac_toe',
-      deployed: 'https://youliketictactoe.surge.sh/'
-    },
-    {
-      name: 'Treasure Hunter',
-      image: projectImage,
-      github: 'https://github.com/jadlevine/Treasure-Hunter',
-      deployed: 'surge'
-    },
-    {
-      name: "What's in the Fridge",
-      image: projectImage,
-      github: 'https://github.com/jadlevine/whats_in_the_fridge',
-      deployed: 'heroku - n/a'
+      deployed: 'https://youliketictactoe.surge.sh/',
+      description:
+        'A simple game built in VanillaJS, with win/lose/draw condiitons, a computer opponent, and a scoreboard'
     },
     {
       name: 'Full Stack Kingdom',
-      image: projectImage,
-      github: '',
-      deployed: 'heroku'
+      image: FskImg,
+      github: 'https://github.com/jadlevine/Full-Stack-Kingdom',
+      deployed: 'https://full-stack-kingdom-jal.herokuapp.com/',
+      description:
+        'A Full-Stack MERN application for planning a visit to the fictional theme park'
     },
     {
-      name: 'Foliage Friends',
-      image: projectImage,
-      github: '',
-      deployed: 'heroku'
-    },
-    {
-      name: 'GradeBook',
-      image: projectImage,
-      github: '',
-      deployed: 'heroku - n/a'
+      name: 'Seq Keeper',
+      image: SkImg,
+      github: 'https://github.com/jadlevine/seq-keeper',
+      deployed: 'https://seq-keeper.herokuapp.com/',
+      description:
+        'A Full-Stack PERN applicaiton that integrates the searching capabilities of the NCBI and allows users to catalog gene and FASTA sequence data'
     }
   ]
 
   return (
     <div>
-      <div id="projects-container"></div>
       <h1>Projects</h1>
-      {projectsList.map((project) => (
-        <div key={project.name} className="project-card">
-          <h3>{project.name}</h3>
-          <Link to={project.github}>GitHub</Link>
-        </div>
-      ))}
+      <div className="projects-container">
+        {projectsList.map((project) => (
+          <div key={project.name} className="project-card">
+            <div>
+              <img className="project-img" src={project.image}></img>
+              <h2 className="project-name">{project.name}</h2>
+              <p className="project-description">{project.description}</p>
+            </div>
+            <div className="link-row">
+              <Link className="link-out" to={project.github}>
+                GitHub
+              </Link>
+              <div className="link-out divider">|</div>
+              <Link className="link-out" to={project.deployed}>
+                App
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
